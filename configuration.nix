027ac2226ed9419d0165f93c437309bc116af715
ozzly <gpg@ozzly.net>
2025-10-18 23:39:24 +0100

    comment cleanup, grouping code

--- configuration.nix
+++ configuration.nix
@@ -6,30 +6,17 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.luks.devices."luks-66da0df4-9177-48b0-a647-5030f652128e".device = "/dev/disk/by-uuid/66da0df4-9177-48b0-a647-5030f652128e";
   
   networking.hostName = "nixos";
-  # Enable networking
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
   i18n.extraLocales = [ "ja_JP.UTF-8/UTF-8" ];
-  #i18n.extraLocaleSettings = {
-  #  LC_ADDRESS = "en_GB.UTF-8";
-  #  LC_IDENTIFICATION = "en_GB.UTF-8";
-  #  LC_MEASUREMENT = "en_GB.UTF-8";
-  #  LC_MONETARY = "en_GB.UTF-8";
-  #  LC_NAME = "en_GB.UTF-8";
-  #  LC_NUMERIC = "en_GB.UTF-8";
-  #  LC_PAPER = "en_GB.UTF-8";
-  #  LC_TELEPHONE = "en_GB.UTF-8";
-  #  LC_TIME = "en_GB.UTF-8";
-  #};
-
+  console.keyMap = "uk";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
@@ -45,35 +32,14 @@
       user = "syl";
     };
   }; 
-  #services.displayManager.sddm.wayland.enable = true;
-  #services.displayManager.sddm.enable = true;
-  # Enable automatic login for the user.
-  #services.displayManager.autoLogin.enable = true;
-  #services.displayManager.autoLogin.user = "syl";
-
-  # Configure console keymap
-  console.keyMap = "uk";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
 
-
-  
-
-  
-  # Setup user syl 
   users.users.syl = {
     shell = pkgs.zsh;
     isNormalUser = true;
     description = "syl";
     extraGroups = [ "networkmanager" "wheel" ];
   };
-
-
-
-
-
 
   environment.systemPackages = with pkgs; [
     vim
@@ -93,7 +59,6 @@
     signal-desktop
     syncthing
     pavucontrol
-    tutanota-desktop
     vscode
     nodejs
     tree
@@ -104,7 +69,6 @@
     qbittorrent
     brave
     vlc
-    phinger-cursors
     gimp
     scrcpy
     kdePackages.kdenlive
@@ -112,15 +76,16 @@
     caligula
   ];
 
+  services.printing.enable = true;
+  services.mullvad-vpn.enable = true;
   services.gvfs.enable = true;
   programs.localsend.enable = true;
-
-
+  programs.thunar.enable = true;
+  programs.adb.enable = true;
+  programs.steam.enable = true;
+
   # Enabling the thumbnailer
-  environment.pathsToLink = [
-    "share/thumbnailers"
-  ];
-
+  environment.pathsToLink = [ "share/thumbnailers" ];
 
   # Zsh
   programs.zsh = {
@@ -128,22 +93,12 @@
     autosuggestions.enable = true;
   };
 
-
   # Hyprland
   programs.hyprland = {
     enable = true;
     withUWSM = true;
     xwayland.enable = true;
   };
-
-
-
-
-  services.mullvad-vpn.enable = true;
-
-
-
-
 
   # Git
   programs.git = {
@@ -155,9 +110,6 @@
     };
   };
   
-
-  
-
   # GPG
   programs.gnupg.agent = {
     enable = true;
@@ -169,20 +121,10 @@
   services.udev.packages = [ pkgs.yubikey-personalization ];
 
 
-  programs.thunar.enable = true;
-  programs.adb.enable = true;
-
-
-  programs.steam = {
-    enable = true;
-  };
-
-
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     noto-fonts-cjk-sans
   ];
-
 
   # Pipewire
   services.pulseaudio.enable = false;
@@ -194,7 +136,6 @@
     pulse.enable = true;
   };
   
-
   # Enable nvidia driver
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
